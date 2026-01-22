619f4b754b1c1dfa19cfddcd4f69bff949743d81
klesleybr <klesley.pfeitosa@gmail.com>
2026-01-21 22:01:06 -0300

    build: modifies the firebase config

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 export const firebaseConfig = {
 
@@ -12,3 +13,4 @@ export const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
